a15a636381db4284735f256be59abe04daa641df
Shyonokaze <vinceyuhaopan@sina.com>
2017-07-27 15:51:45 +0800

    Update verscrollbar.py

--- verscrollbar.py
+++ verscrollbar.py
@@ -8,9 +8,9 @@ def color():
     Beijing_counter=0
     for choice in Beijing.keys():
         if Beijing[choice][0]:
-            Beijing_lb.itemconfig(Beijing_counter,fg='black')
+            Beijing_lb.itemconfig(Beijing_counter,fg='black',selectforeground='white')
         else:
-            Beijing_lb.itemconfig(Beijing_counter,fg='red')
+            Beijing_lb.itemconfig(Beijing_counter,fg='red',selectforeground='Indigo')
         Beijing_counter +=1
 
 def deleter(event):
@@ -18,10 +18,10 @@ def deleter(event):
     color()
     Beijing_best.config(text='最佳房源：%s'%findbest())
     if Beijing[Beijing_lb.get(Beijing_lb.curselection())][0]:
-        last_recover.config(text='上一个恢复的房源：%s'%Beijing_lb.get(Beijing_lb.curselection()))
+        mem_lb.insert(tk.END,'恢复房源%s'% Beijing_lb.get(Beijing_lb.curselection()))
     else:
-        last_delete.config(text='上一个删除的房源：%s'%Beijing_lb.get(Beijing_lb.curselection()))
-    
+        mem_lb.insert(tk.END,'删除房源%s'%Beijing_lb.get(Beijing_lb.curselection()))
+    mem_lb.yview(tk.END)
 
 def findbest():
     bestchoice='无'
@@ -61,9 +61,10 @@ def reset():
         for choice in Beijing.keys():
             Beijing[choice][0]=1
         color()
-        last_delete.config(text='上一个删除的房源：无')
-        last_recover.config(text='上一个恢复的房源：无')
+
         Beijing_best.config(text='最佳房源：%s'%findbest())
+        mem_lb.delete(0,tk.END)
+        
         
         
         
@@ -129,13 +130,13 @@ Beijing_lb = tk.Listbox(top,font=('微软雅黑',15),height = 20)
 Beijing_sl = tk.Scrollbar(top,command=Beijing_lb.yview)
 Beijing_best = tk.Label(top,text='最佳房源：%s'%findbest(),font=('微软雅黑',15))
 Beijing_best.grid(row = 0,column = 0,columnspan=2)
-last_delete = tk.Label(top,text='上一个删除的房源：无',font=('微软雅黑',10))
-last_delete.grid(row = 3,column = 0,columnspan=2,sticky='W')
-last_recover = tk.Label(top,text='上一个恢复的房源：无',font=('微软雅黑',10))
-last_recover.grid(row = 4,column = 0,columnspan=2,sticky='W')
 Beijing_lb.config(yscrollcommand = Beijing_sl.set)
-Beijing_lb.grid(row = 1,column = 0,rowspan=2,sticky='NSE')
-Beijing_sl.grid(row = 1,column = 1,rowspan=2,sticky='NSW')
+Beijing_lb.grid(row = 1,column = 0,sticky='NSEW')
+Beijing_sl.grid(row = 1,column = 1,sticky='NSW')
+
+mem_lb = tk.Listbox(top,font=('微软雅黑',10),height = 4)
+mem_lb.grid(row = 2,column = 0,sticky='EW')
+
 for choice in Beijing.keys():
     Beijing_lb.insert(tk.END,choice)
 color()
@@ -145,8 +146,8 @@ Beijing_lb.bind('<Double-Button-1>',deleter)
 
 tk.Button(top, text = "打印已选",font=("华文彩云", 15),command = printnew).grid(row = 1,column = 999,sticky='S')
 
-tk.Button(top, text = "保存",font=("华文彩云", 15),command = save).grid(row = 0,column = 999)
-tk.Button(top, text = "重置",font=("华文彩云", 15),command = reset).grid(row = 3,column = 999,rowspan=2)
+tk.Button(top, text = "保存",font=("华文彩云", 15),command = save).grid(row = 2,column = 999)
+tk.Button(top, text = "重置",font=("华文彩云", 15),command = reset).grid(row = 0,column = 999)
         
     
 top.mainloop()
